53aa9cd3efadd4dc2d0fefa9731766d45188b8a1
José Luis León Carrión <38015331+joseleonec@users.noreply.github.com>
2026-06-24 16:36:39 -0500

    Crud cliente (#1)

---
+++ src/main/java/com/devsu/banking/config/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.devsu.banking.config.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Object id) {
+        super(resource + " not found with id: " + id);
+    }
+}
